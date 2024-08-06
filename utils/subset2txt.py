b91a6f23342cc20776052d8b3389b42af89a73fd
huangzhe <huangzhe@jbufa.com>
2024-08-06 11:30:53 +0800

    add tool subset2txt

---
+++ utils/subset2txt.py
@@ -0,0 +1,42 @@
+import argparse
+import glob
+import logging
+import os
+
+from utils import set_logging
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir_root_in',
+                        default='/run/user/1000/gvfs/smb-share:server=172.20.254.200,share=sharedfolder/Test/fire')
+    parser.add_argument('--dir_root_out',
+                        default='/home/Huangzhe/Test/fire')
+    parser.add_argument('--subset', default='val')
+    return parser.parse_args()
+
+
+def run(args):
+    image_extensions = ['*.jpg', '*.jpeg', '*.png', '*.gif', '*.bmp', '*.tiff']
+    image_paths = []
+    root_imgs = os.path.join(args.dir_root_in, 'images')
+    image_dir = os.path.join(root_imgs, args.subset)
+    output_txt_file = os.path.join(args.dir_root_in, args.subset + '.txt')
+    for ext in image_extensions:
+        for image_path in glob.glob(os.path.join(image_dir, '**', ext), recursive=True):
+            image_path_out = image_path.replace(args.dir_root_in, args.dir_root_out)
+            image_paths.append(image_path_out)
+    with open(output_txt_file, 'w') as f:
+        for path in image_paths:
+            f.write(path + '\n')
+
+
+def main():
+    set_logging()
+    args = parse_args()
+    logging.info(args)
+    run(args)
+
+
+if __name__ == '__main__':
+    main()
---
+++ utils/utils.py
@@ -0,0 +1,16 @@
+import logging
+import os
+import shutil
+
+
+def set_logging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+
+def make_dirs(dir_root, reset=False):
+    if dir_root is None:
+        return
+    if os.path.exists(dir_root) and reset:
+        shutil.rmtree(dir_root)
+    os.makedirs(os.path.join(dir_root), exist_ok=True)
